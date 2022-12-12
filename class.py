604907fb9fe6ddc415150b3aca40a034745a767c
George Barrett <georgeaustinbarrett@gmail.com>
2022-12-12 12:51:07 +0000

    basic modules objects and classes complete

---
+++ class.py
@@ -0,0 +1,10 @@
+class my_stuff(object):
+    def __init__(self):
+        self.tangerine = 'And now a thousand years between us.'
+
+    def apple(self):
+        print('I\'m a classy apple.')
+
+thing = my_stuff
+thing.apple()
+print(thing.tangerine)
---
+++ modules-objects.py
@@ -0,0 +1,10 @@
+import my_stuff
+
+my_stuff.apple()
+
+print(my_stuff.tangerine)
+
+my_stuff['apple']
+my_stuff.apple()
+my_stuff.tangerine
+
