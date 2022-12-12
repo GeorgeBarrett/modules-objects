5f1f6e45cf970efd5c26e0e3efb8ccdf3b4ee391
George Barrett <georgeaustinbarrett@gmail.com>
2022-12-12 12:59:52 +0000

    modules, objects, classes complete. with comments

--- class.py
+++ class.py
@@ -1,10 +1,19 @@
+# creating a class
 class my_stuff(object):
+    
+    # a function that prints a message
     def __init__(self):
         self.tangerine = 'And now a thousand years between us.'
 
+    # another function that prints a message
     def apple(self):
         print('I\'m a classy apple.')
 
-thing = my_stuff
+# this line instantiates the my_stuff() class into an object
+thing = my_stuff()
+
+# now i can call my class 'thing' and fire the apple function 
 thing.apple()
+
+# now i can call my class thing and fire the variable that stores a print message
 print(thing.tangerine)
